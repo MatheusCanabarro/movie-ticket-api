dea603446b6cffb523bba4cdd1eb842e2f2f6c1e
matheus_canabarro <matheus_canabarro@sicredi.com.br>
2022-08-20 21:02:23 -0300

    Initial commit.

---
+++ src/test/java/matheus/canabarro/movieticketapi/MovieTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package matheus.canabarro.movieticketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
